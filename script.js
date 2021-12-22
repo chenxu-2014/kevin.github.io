a619b9dd04aa96b5e8d281837df0761f232b14d5
chenxu <45989849+chenxu-2014@users.noreply.github.com>
2021-12-22 19:05:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var app = angular.module('simpleList', ['ui.sortable']);
 
 app.controller('sort', function($scope) {
-  var items = [{name:'Milk'}, {name:'Eggs'}, {name:'Fabric Softener'}];
+  var items =[{name:'October is a great month... '}, 
+              {name:'We have some interesting and fun things for you this term. '}, 
+              {name:'This is a really busy term! Have a good time!'},
+              {name:'Next month, we have ...'},
+              {name:'And on December 3rd,we have a book sale...'},];
   $scope.list = items;
 });
